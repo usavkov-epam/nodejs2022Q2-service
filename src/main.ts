cac6c7b5d8174e1288ca79e43ca4715bdb930b05
Yury Saukou <yury_saukou@epam.com>
2022-07-21 23:27:03 +0400

    Update OpenAPI and add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { cwd } from 'process';
+
 import 'dotenv/config';
 
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
 
 import { AppModule } from './app.module';
 
@@ -9,7 +15,14 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const filepath = join(cwd(), 'doc', 'api.yaml');
+  const document = await readFile(filepath, 'utf-8').then(parse);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
   });
