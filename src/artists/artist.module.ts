940a6175cdd3c0354a13c17dbdd0119c4ea79940
Yury Saukou <yury_saukou@epam.com>
2022-07-31 23:15:41 +0400

    Connect artist service with db (without relations)

--- src/artists/artist.module.ts
+++ src/artists/artist.module.ts
@@ -1,16 +1,19 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AlbumsModule } from '../albums';
 import { FavoritesModule } from '../favorites';
 import { TrackModule } from '../tracks';
 import { ArtistController } from './artist.controller';
 import { ArtistService } from './artist.service';
+import { ArtistEntity } from './entities';
 
 @Module({
   imports: [
     forwardRef(() => AlbumsModule),
     forwardRef(() => FavoritesModule),
     forwardRef(() => TrackModule),
+    TypeOrmModule.forFeature([ArtistEntity]),
   ],
   controllers: [ArtistController],
   providers: [ArtistService],
